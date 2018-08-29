9a87c4c7a5cd965cfe7ed5ffb8155db0ac1041d5
pergola <lolpsxvipkry2@gmail.com>
2018-08-28 22:14:52 -0300

    adicinando primeira versão do MPI minimamente funcional

--- main.c
+++ main.c
@@ -5,9 +5,8 @@
 #include <string.h>
 #include "mpi.h"
 
-int main(int argc, char *argv[])
-{
-    MPI_Init (NULL, NULL);
+int main(int argc, char** argv)    {
+    MPI_Init (&argc, &argv);
     int comm_rank = 0;
     int comm_size = 0;
 
@@ -15,10 +14,15 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
     MPI_Status mpi_status;
 
-    if(comm_rank == 0){
+    char msg[100];
 
+    if(comm_rank == 0){
+        printf("Sou a master!\n");
+        // for(int i = 0; i < comm_size; i++){
+        //     MPI_Recv(msg, 100, MPI_CHAR, i, 0, MPI_COMM_WORLD, &mpi_status);
+        // }
     }else { // is slave
-        
+        printf("Sou o processo %d!\n", comm_rank);
     }
 
     MPI_Finalize();
